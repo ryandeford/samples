2f67ae74cea6865d1d2b721205d51fc378447147
ryandeford <ryandeford@gmail.com>
2023-12-22 09:45:23 -0500

    #17: Removing caching features from code/tests for Block Party's application code challenge: it wasn't a good idea to leave in place, as it wasn't providing a performance boost (it was actually hurting it), and it was potentially dangerous from a memory consumption perspective: see https://github.com/ryandeford/samples/commit/2e62bf794ba952778129c34f421e506d59b99d40#r135567214 for details

--- playgrounds/KotlinPlayground/src/io/ryandeford/solutions/SolutionMaxPairIndices.kt
+++ playgrounds/KotlinPlayground/src/io/ryandeford/solutions/SolutionMaxPairIndices.kt
@@ -8,18 +8,13 @@ package io.ryandeford.solutions
  * case of a tie, the method should return the leftmost pair (e.g., maxPair([0, 4, 3, 1, 2, 3, 4, 0]) will return (1, 2)).
  */
 class SolutionMaxPairIndices {
-    // We'll use this to store static constants/configurations, predefined results, and a map to cache results
+    // We'll use this to store static error instances and predefined results
     // Note: We're also making the sample maxPair method static so that it's easy to access without object instantiation
     // Note: In real life, we'd generally avoid statics and utilize dependency injection frameworks to wire in configurations/implementations
     companion object {
         private val ERROR_FOR_NULL_INPUT = IllegalArgumentException("The input cannot be null")
         private val ERROR_FOR_INPUT_OF_INVALID_SIZE = IllegalArgumentException("The input must contain at least two values")
-
         private val RESPONSE_FOR_INPUT_OF_SIZE_2 = Pair(0,1)
-
-        // Note: Discuss with team if caching is desired/necessary, and consider adding better configs/controls to disable caching, limit size, etc.
-        private const val CACHING_ENABLED = true
-        private val INPUT_TO_RESPONSE_CACHE = mutableMapOf<String, Pair<Int,Int>>()
 
         // Note: Kotlin has neat built-in features for null safety, and if we wanted, we could force this method to not accept null inputs
         // Note: Discuss method contracts with the team to see what the best option is, for now we're allowing null inputs
@@ -41,11 +36,6 @@ class SolutionMaxPairIndices {
             if (input.size == 2) {
                 return RESPONSE_FOR_INPUT_OF_SIZE_2
             }
-            // If caching is enabled and the input has been seen before with the result already cached, return the cached result
-            // WARNING: This could potentially be dangerous if there are massive amounts of input/output pairs being cached, so again, discuss with team
-            if (CACHING_ENABLED && INPUT_TO_RESPONSE_CACHE.contains(getHashKey(input))) {
-                return INPUT_TO_RESPONSE_CACHE.getValue(getHashKey(input));
-            }
 
             // Setup mutable variables to store the highest sum of consecutive values along with the indices
             // Note: At this point, we already know there are at least 2 input values present, so we'll set the initial condition for max sum/indices directly
@@ -64,21 +54,7 @@ class SolutionMaxPairIndices {
                 }
             }
 
-            if (CACHING_ENABLED) {
-                INPUT_TO_RESPONSE_CACHE[getHashKey(input)] = currentMaxConsecutiveSumIndices
-            }
-
             return currentMaxConsecutiveSumIndices;
-        }
-
-        // This is a simple way to get an easy-to-use hash code for IntArray instances
-        fun getHashKey(input: IntArray?): String {
-            return when(input) {
-                // If the input is null, just return the string "null"
-                null -> "null"
-                // Otherwise return all the items of the array joined with a comma
-                else -> input.joinToString(separator = ",")
-            }
         }
     }
 }
